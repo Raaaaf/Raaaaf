f2c90851db76c0dd51f55ddb2e4d4f7aa53fb6dc
ÐJ RΔFΔEĿE ™ <82766347+Raaaaf@users.noreply.github.com>
2021-04-18 17:14:57 +0200

    Update and rename client to client.php

---
+++ client.php
@@ -0,0 +1,10 @@
+<script type="text/javascript"> 
+		var habboName = "{username}";
+		var habboReqPath = "{url}";
+		var habboStaticFilePath = "http://images.habbo.com/habboweb/{web_build}/web-gallery";
+		var habboImagerUrl = "http://www.habbo.com/habbo-imaging/";
+		var habboPartner = "";
+		var habboDefaultClientPopupUrl = "{url}/client";
+		window.name = "ClientWndw";
+		if (typeof HabboClient != "undefined") { HabboClient.windowName = "ClientWndw"; }
+	</script> 
